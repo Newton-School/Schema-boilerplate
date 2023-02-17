2896dbd2ca5b6eebb7a4c5e50e2a1b91bbb8c968
swaraj-jain <happy2000jain@gmail.com>
2023-02-17 16:49:53 +0530

    Boiler Plate added

---
+++ models/product.js
@@ -0,0 +1,24 @@
+const { number } = require("joi");
+var mongoose= require("mongoose");
+
+var productSchema= mongoose.Schema({
+    name:{
+        type: String,
+        required:true
+    },
+    Quantity:{
+        type: Number,
+        required:true
+    },
+    Price:{
+        type: Number,
+        required:true
+    },
+    instock:{
+        type: String,
+        eneum: ['Yes', 'No']
+    }
+    
+});
+
+module.exports =mongoose.model("products",productSchema);
